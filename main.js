bd9b37bffbf193535a4f8ff6a283985c3df0e5c3
Shivan Modha <shivanmodha@gmail.com>
2017-09-25 21:18:23 -0400

    More Headless Chrome Test

--- main.js
+++ main.js
@@ -1,67 +1,26 @@
 const credentials = require('./credentials.json');
 const { Chromeless } = require('chromeless');
-
-let TestVar = 0;
-
-class Test
-{
-    constructor()
-    {
-        this.TESTVAR = 0;
-        this.run = this.run.bind(this);
-        this.evaluate1 = this.evaluate1.bind(this);
-        this.evaluate2 = this.evaluate2.bind(this);
-    }
-    async run()
-    {
-        const chromeless = new Chromeless({launchChrome: true});
-        const instance = await chromeless.goto("https://students.sbschools.org/genesis/parents?gohome=true")
-            .type(credentials.username + "@sbstudents.org", "input[name='j_username']")
-            .type(credentials.password, "input[name='j_password']")
-            .click("input[class='saveButton']")
-            .click("span[onClick=\"header_goToTab('studentdata&tab2=gradebook','studentid=" + credentials.username + "');\"]")
-            .evaluate(() =>
-            {
-                this.evaluate1();
-                return "";
-                //return document.documentElement.innerHTML;
-            });
-        await chromeless.end();
-        console.log(instance);
-        console.log(this.TESTVAR);
-    }
-    evaluate1()
-    {
-        console.log("YAS");
-    }
-    evaluate2()
-    {
-        return "TEST";
-    }
-}
-let t = new Test();
-t.run().catch(console.error.bind(console));
-/*async function run()
+async function run()
 {
     const chromeless = new Chromeless({launchChrome: true});
     const instance = await chromeless.goto("https://students.sbschools.org/genesis/parents?gohome=true")
         .type(credentials.username + "@sbstudents.org", "input[name='j_username']")
         .type(credentials.password, "input[name='j_password']")
         .click("input[class='saveButton']")
-        .click("span[onClick=\"header_goToTab('studentdata&tab2=gradebook','studentid=" + credentials.username + "');\"]")
-        .evaluate(evaluate1)
-        .evaluate(evaluate2)
+        .wait(1000)
+        .evaluate(() =>
+        {
+            return document.documentElement.innerHTML;            
+        });
     console.log(instance);
-    console.log(TestVar);
+    console.log("****************SEP****************");
+    const instance2 = await chromeless
+        .click("span[onClick=\"header_goToTab('studentdata&tab2=gradebook','studentid=" + credentials.username + "');\"]")
+        .evaluate(() =>
+        {
+            return document.documentElement.innerHTML;            
+        });
+    console.log(instance2);
     await chromeless.end();
 }
-function evaluate1()
-{
-    TestVar = 2;
-    return document.documentElement.innerHTML;
-}
-function evaluate2()
-{
-    return "TEST";
-}
-run().catch(console.error.bind(console))*/
+run().catch(console.error.bind(console))
