62c71d7ac6a3c88f2e2512b38661c708ccb90c6b
Shivan Modha <shivanmodha@gmail.com>
2017-09-27 12:31:43 -0400

    Multiple Pages Support

--- main.js
+++ main.js
@@ -3,22 +3,25 @@ const { Chromeless } = require('chromeless');
 async function run()
 {
     const chromeless = new Chromeless({launchChrome: true});
-    const instance = await chromeless.goto("https://students.sbschools.org/genesis/parents?gohome=true")
+    const instance = await chromeless
+        .goto("https://students.sbschools.org/genesis/parents?gohome=true")
         .type(credentials.username + "@sbstudents.org", "input[name='j_username']")
         .type(credentials.password, "input[name='j_password']")
         .click("input[class='saveButton']")
         .wait(1000)
         .evaluate(() =>
         {
-            return document.documentElement.innerHTML;            
+            return document.documentElement.innerHTML;
         });
     console.log(instance);
     console.log("****************SEP****************");
     const instance2 = await chromeless
+        .goto("https://students.sbschools.org/")
         .click("span[onClick=\"header_goToTab('studentdata&tab2=gradebook','studentid=" + credentials.username + "');\"]")
+        .wait(1000)
         .evaluate(() =>
         {
-            return document.documentElement.innerHTML;            
+            return document.documentElement.innerHTML;
         });
     console.log(instance2);
     await chromeless.end();
